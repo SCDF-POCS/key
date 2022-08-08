0bcbdbc4328dbc3239ccf8ef843993047265c1d2
SCDF-POCS <53817636+SCDF-POCS@users.noreply.github.com>
2022-08-08 14:40:48 +0530

    Add files via upload

---
+++ azurekeyvaultWithManagedService/src/main/java/com/javainuse/KeyVaultSecretService.java
@@ -0,0 +1,24 @@
+package com.javainuse;
+
+ 
+
+import com.azure.security.keyvault.secrets.SecretClient;
+
+public class KeyVaultSecretService {
+
+	private final SecretClient secretClient;
+
+	public KeyVaultSecretService(SecretClient secretClient) {
+		this.secretClient = secretClient;
+	}
+
+	public String getValue(String key) {
+		var keyVaultValue = secretClient.getSecret(key);
+
+		if (keyVaultValue == null)
+			return "null";
+		else
+			return keyVaultValue.getValue();
+	}
+
+}
---
+++ azurekeyvaultWithManagedService/src/main/java/com/javainuse/KeyvaultApplication.java
@@ -0,0 +1,35 @@
+package com.javainuse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.azure.identity.ManagedIdentityCredential;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+
+@SpringBootApplication
+public class KeyvaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyvaultApplication.class, args);
+    }
+    
+   
+  //  private String connectionString = "defaultValue\n";
+
+	
+	/*
+	 * @Bean public ManagedIdentityCredential dbCredential() {
+	 * ManagedIdentityCredential managedIdentityCredential = new
+	 * ManagedIdentityCredentialBuilder() .build(); return
+	 * managedIdentityCredential; }
+	 */
+	/*
+	 * public void run(String... varl) throws Exception { System.out.println(String.
+	 * format("\nConnection String stored in Azure Key Vault:\n%s\n",
+	 * connectionString)); }
+	 */
+
+}
